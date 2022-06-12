1e07337c6b45cce747b45037a7305aad9e800ceb
Mikhail Laptev <lapteff77@yandex,ru>
2022-06-13 00:00:47 +0300

    Sprint2v.0

--- src/test/java/CateTest.java
+++ src/test/java/CateTest.java
@@ -1,12 +1,12 @@
 import com.example.Cat;
-import com.example.Feline;
+
 import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 
-import java.util.List;
+
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.notNullValue;
+
 
 public class CateTest {
 
